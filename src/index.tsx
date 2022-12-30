0f554a3d05aa95477c76ac1c2a962a1729e426ce
Phạm Mạnh Kiên <kien.pham@bmbsoft.com.vn>
2022-12-30 15:50:21 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/es/integration/react';
+/// <reference types="redux-persist" />
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
